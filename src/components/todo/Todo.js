0c3c8a1ec7b6595e4e20db816566ad8d1a4aa3a5
Vinh DOAN <dhqvinh@gmail.com>
2020-07-28 04:35:34 +1200

    Used react hook

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,90 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 import TodoItem from './TodoItem';
 
-export default class Todo extends Component {
-  constructor(props) {
-    super(props);
-
-    // Initial state
-    this.state = {
-      todos: [
-        {
-          id: 1,
-          text: 'Todo item 01'
-        },
-        {
-          id: 2,
-          text: 'Todo item 02'
-        },
-      ],
-      todoText: ''
-    };
-  }
+const Todo = () => {
+  // Initial state
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Todo item 01' },
+    { id: 2, text: 'Todo item 02' }
+  ]);
+  const [todoText, setTodoText] = useState('');
 
   // Methods & handlers
-  generateId() {
-    if (this.state.todos && this.state.todos.length > 0) {
-      return Math.max(...this.state.todos.map(item => item.id)) + 1;
+  const generateId = () => {
+    if (todos && todos.length > 0) {
+      return Math.max(...todos.map(item => item.id)) + 1;
     } else {
       return 1;
     }
-  }
+  };
 
-  createTodo = () => {
+  const createTodo = () => {
     // Validate todoText
-    if (!this.state.todoText) {
+    if (!todoText) {
       alert('Please enter a todo!');
       return;
     }
 
-    const newTodo = { id: this.generateId(), text: this.state.todoText };
-
-    this.setState(({ todos }) => ({
-      todos: [
-        ...todos,
-        newTodo
-      ],
-      todoText: ''
-    }));
+    const newTodo = { id: generateId(), text: todoText };
+    setTodos([...todos, newTodo]);
+    setTodoText('');
   };
 
 
-  handleInputKeyPress = (e) => {
+  const handleInputKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.createTodo();
+      createTodo();
     }
   };
 
-  handleInputChange = (e) => {
-    this.setState({
-      todoText: e.target.value
-    });
+  const handleInputChange = (e) => {
+    setTodoText(e.target.value);
   };
 
 
-  deleteTodo = (id) => {
-    this.setState(({ todos }) => ({
-      todos: todos.filter((item) => item.id !== id)
-    }));
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
   };
 
-  render() {
-    return (
-      <div className="Todo">
-        <h1 className="Todo-header">React To Do</h1>
-        <div className="Todo-container">
-          <div className="Todo-content">
-            {this.state.todos.map(item => {
-              return <TodoItem key={item.id} item={item} deleteItem={this.deleteTodo} />
-            })}
-          </div>
-          <div className="Todo-input">
-            <input className="Todo-input-text" type="text" value={this.state.todoText} onChange={this.handleInputChange} onKeyPress={this.handleInputKeyPress} />
-            <button className="Todo-input-add" onClick={this.createTodo}>+</button>
-          </div>
+  return (
+    <div className="Todo">
+      <h1 className="Todo-header">React To Do</h1>
+      <div className="Todo-container">
+        <div className="Todo-content">
+          {todos.map(item => {
+            return <TodoItem key={item.id} item={item} deleteItem={deleteTodo} />
+          })}
+        </div>
+        <div className="Todo-input">
+          <input className="Todo-input-text" type="text" value={todoText} onChange={handleInputChange} onKeyPress={handleInputKeyPress} />
+          <button className="Todo-input-add" onClick={createTodo}>+</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Todo;
--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './TodoItem.css';
 
-export default class TodoItem extends Component {
+const TodoItem = (props) => {
+  const { item, deleteItem } = props;
 
-  render() {
-    const { item, deleteItem } = this.props;
+  return (
+    <div className="TodoItem">
+      <span className="TodoItem-text">{item.text}</span>
+      <button className="TodoItem-delete" onClick={() => deleteItem(item.id)}>-</button>
+    </div>
+  )
+};
 
-    return (
-      <div className="TodoItem">
-        <span className="TodoItem-text">{item.text}</span>
-        <button className="TodoItem-delete" onClick={() => deleteItem(item.id)}>-</button>
-      </div>
-    )
-  }
-}
+export default TodoItem;
